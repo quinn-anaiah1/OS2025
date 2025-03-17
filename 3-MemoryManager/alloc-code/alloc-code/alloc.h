2f229d6f7cb00e3564f0b2be4844e14e81598e38
quinn-anaiah1 <aequinn@miners.utep.edu>
2025-03-16 22:04:52 -0600

    function descriptions

--- 3-MemoryManager/alloc-code/alloc-code/alloc.h
+++ 3-MemoryManager/alloc-code/alloc-code/alloc.h
@@ -8,7 +8,29 @@
 #define MINALLOC 8 //allocations will be 8 bytes or multiples of it
 
 // function declarations
-int init_alloc();
-int cleanup();
+
+/*The function init alloc() must initialize the memory manager, including
+allocating a 4KB page from the OS via mmap, and initializing any other data
+structures required. This function will be invoked by the user before requesting
+any memory from your memory manager. This function must return 0 on
+success and a non-zero error code otherwise. */
+int init_alloc(); \
+
+
+/*The function cleanup() must cleanup state of your manager, and return the
+memory mapped page back to the OS. This function must return 0 on success
+and a non-zero error code otherwise. */
+int cleanup(); 
+
+
+/*The function alloc(int) takes an integer bu_er size that must be allocated
+and returns a char * pointer to the bu_er on a success. This function returns a
+NULL on failure (e.g., requested size is not a multiple of 8 bytes, or insu_icient
+free space). When successful, the returned pointer should point to a valid
+memory address within the 4KB page of the memory manager*/
 char *alloc(int);
+
+
+/*The function dealloc(char *) takes a pointer to a previously allocated
+memory chunk, and frees up the entire chunk.*/
 void dealloc(char *);
