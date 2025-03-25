74c0205ef354252e45f54020f7b71306c4d63d47
quinn-anaiah1 <aequinn@miners.utep.edu>
2025-03-25 13:00:23 -0600

    inclass week 8

---
+++ Inclass/myProg.c
@@ -0,0 +1,29 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //Header file for sleep(). man 3 sleep for details.
+ 
+ 
+// A normal C function that is executed as a thread
+// when its name is specified in pthread_create()
+void* myThreadFun(void* threadNum)
+{
+    int num = (int *) threadNum;
+    printf("Printing Thread Num: %d \n", threadNum);
+    sleep(1);
+    return NULL;
+}
+ 
+ 
+int main()
+{
+    pthread_t thread_id;
+    int i= 0; 
+    int N; //ask user for how many threads
+    printf("Before Thread\n");
+    pthread_create(&thread_id, NULL, myThreadFun, &i);
+    pthread_join(thread_id, NULL);
+    printf("After Thread\n");
+    exit(0);
+}
+ 
