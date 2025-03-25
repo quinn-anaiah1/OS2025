df9497dfdb41a2cc5e7d21b13a1a41e43550e30e
quinn-anaiah1 <aequinn@miners.utep.edu>
2025-03-25 13:35:31 -0600

    no locks

--- Inclass/myProg.c
+++ Inclass/myProg.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> //Header file for sleep(). man 3 sleep for details.
- 
- 
+
+int NUM_THREADS; // global variable storing number of threads
+
 // A normal C function that is executed as a thread
 // when its name is specified in pthread_create()
 void* myThreadFun(void* threadNum)
@@ -17,11 +18,8 @@ void* myThreadFun(void* threadNum)
  // each thread should print its own thread id 
 int main()
 {
-    pthread_t thread_id;
-
-    int i= 0; 
     int N;
-    
+    // get the number of threads to create from user 
     printf("Enter the number of threads: ");
     if (scanf("%d", &N) != 1) {
         printf("Invalid input. Please enter a valid integer.\n");
@@ -29,9 +27,29 @@ int main()
     }
     
     printf("Number of thread: %d \n", N);
+    NUM_THREADS = N;
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
+
+    int i= 0; 
+    
+    /*Create N threads*/
+    for(int i = 0; i < NUM_THREADS; i++){
+        thread_ids[i] = i;
+        if (pthread_create(&threads[i], NULL, myThreadFun, &thread_ids[i]) != 0) {
+            perror("Failed to create thread");
+            return 1;
+        }
+    }
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    /*Joing N threads*/
+    
     printf("Before Thread\n");
-    pthread_create(&thread_id, NULL, myThreadFun, "0");
-    pthread_join(thread_id, NULL);
+    
+    
     printf("After Thread\n");
     exit(0);
 }
